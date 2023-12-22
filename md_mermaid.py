dc24ab6526de23553baeb401aafc749cd47346b9
Kristinita <vspobog@gmail.com>
2023-12-22 08:38:54 +0300

    fix(regex): escape backslash to prevent warning “SyntaxWarning: invalid escape sequence '\ '”
    https://stackoverflow.com/a/52335971/5951529

--- md_mermaid.py
+++ md_mermaid.py
@@ -1,5 +1,4 @@
-"""
-Mermaid Extension for Python-Markdown
+"""Mermaid Extension for Python-Markdown.
 ========================================
 
 Adds mermaid parser (like github-markdown) to standard Python-Markdown code blocks.
@@ -10,15 +9,12 @@ License: GNU GPLv3
 
 """
 
+
+import re
+
 from markdown.extensions import Extension
 from markdown.preprocessors import Preprocessor
 
-import re
-import string
-
-
-def strip_notprintable(myStr):
-    return ''.join(filter(lambda x: x in string.printable, myStr))
 
 MermaidRegex = re.compile(
     r"^(?P<mermaid_sign>[\~\`]){3}[\ \t]*[Mm]ermaid[\ \t]*$")
@@ -42,7 +38,7 @@ class MermaidPreprocessor(Preprocessor):
             if not in_mermaid_code:
                 m_start = MermaidRegex.match(line)
             else:
-                m_end = re.match(r"^[" + mermaid_sign + "]{3}[\ \t]*$", line)
+                m_end = re.match(r"^[" + mermaid_sign + "]{3}[\\ \t]*$", line)
                 if m_end:
                     in_mermaid_code = False
 
@@ -53,7 +49,9 @@ class MermaidPreprocessor(Preprocessor):
                     new_lines.append("")
                 if not is_mermaid:
                     is_mermaid = True
-                    #new_lines.append('<style type="text/css"> @import url("https://cdn.rawgit.com/knsv/mermaid/0.5.8/dist/mermaid.css"); </style>')
+                    # new_lines.append('<style type="text/css">
+                    # @import url("https://cdn.rawgit.com/knsv/mermaid/0.5.8/dist/mermaid.css");
+                    # </style>')
                 new_lines.append('<pre class="mermaid">')
                 m_start = None
             elif m_end:
@@ -69,10 +67,10 @@ class MermaidPreprocessor(Preprocessor):
 
 
 class MermaidExtension(Extension):
-    """ Add source code hilighting to markdown codeblocks. """
+    """Add source code hilighting to markdown codeblocks."""
 
     def extendMarkdown(self, md):
-        """ Add HilitePostprocessor to Markdown instance. """
+        """Add HilitePostprocessor to Markdown instance."""
         # Insert a preprocessor before ReferencePreprocessor
         md.preprocessors.register(MermaidPreprocessor(md), 'mermaid', 35)
 
